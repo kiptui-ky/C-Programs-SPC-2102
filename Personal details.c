15fa676c4edfdd581222ba7f403f14c1feefd29a
Kiptui Kipyator PA106/G/28740/25 <kiptuizaccpi@gmail.com>
2025-09-21 01:18:25 +0300

    Add files via upload

---
+++ Personal details.c
@@ -0,0 +1,27 @@
+// Simple Hello Wolrd Program
+/*
+Name: Kipyator Kiptui
+Reg no: PA106/G/28740/25;
+}Description: Program to display Hello World
+*/
+#include<stdio.h> //Preprocessor directive: scanf(),
+
+//main function
+int main() {
+int height;
+    printf("Enter your height: ");
+    scanf("%a", & height);
+    printf("Your height is %a ",height);
+    
+int number;    
+    printf("Enter your Id number: ");
+    scanf("%a", & number);
+    printf("Your number is %a ", number);
+
+int balance;
+    printf("Enter your bank balance: ");
+    scanf("%a", & balance);
+    printf("Your balance is %a ", balance);
+    
+    return 0;
+}
---
+++ Semister units .c
@@ -0,0 +1,20 @@
+// Simple Hello Wolrd Program 
+/*
+Name: Kipyator Kiptui 
+Reg no: PA106/G/28740/25;
+}Description: Program to display Hello World
+*/
+#include<stdio.h> //Preprocessor directive: scanf(),
+
+//main function 
+int main() {
+    printf("SPC 2101 Introduction to Programming and Problem Solving \n");
+    printf("SPI 2102 Computer Systems and Organization \n");
+    printf("SPM 2100 Discrete Mathematics \n");
+    printf("SPM 2104 Mathematics for Science \n");
+    printf("SSE 2101 Fundamentals of Sytems Analysis and Design \n");
+    printf("UCU 2105 Fundementals of ICT \n");
+    printf("UCU 2106 Communication and Information Literacy Skills \n");
+    
+    return 0;
+    }
